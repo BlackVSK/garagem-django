2c56f925e292075173350f1623c7aec98a2182ff
BlackVSK <phrod2007@gmail.com>
2025-05-23 10:56:18 -0300

    Criação da model Acessorio,Cor e Veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,33 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name="Acessório"
+        verbose_name_plural="Acessórios"
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name="Cor"
+        verbose_name_plural="Cores"
+
+class Veiculo(models.Model):
+    ano = models.IntegerField(default=0, blank=True, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=0)
+    Marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos", blank=True, null=True)
+    Categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos", blank=True, null=True)
+    Cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.Marca}, {self.Categoria}, {self.ano}, {self.Cor}"
