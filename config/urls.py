eb16644a125db46e7a8106e067ff16759789975c
BlackVSK <phrod2007@gmail.com>
2025-05-26 17:29:22 -0300

    Exercicio da aula 8 concluido

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from garagem.views import MarcaViewSet
+from garagem.views import MarcaViewSet, CorViewSet, AcessorioViewSet, CategoriaViewSet, VeiculoViewSet
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
+router.register(r"cores", CorViewSet)
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"veiculos", VeiculoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- garagem/views.py
+++ garagem/views.py
@@ -1,7 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
-from garagem.serializers import MarcaSerializer
-from garagem.models import Marca
+from garagem.serializers import MarcaSerializer, CategoriaSerializer, AcessorioSerializer, CorSerializer, VeiculoSerializer, VeiculoDetailsSerializer, VeiculoListSerializer
+from garagem.models import Marca, Categoria, Acessorio, Cor, Veiculo
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    def get_serializer_class(self):
+        if self.action == "list":
+            return VeiculoListSerializer
+        elif self.action == "retrieve":
+            return VeiculoDetailsSerializer
+        return VeiculoSerializer
